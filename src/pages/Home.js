3439a90c76f2b454ab24ee27654f49ed3de454e5
iyasmzn <iyasmzn07@gmail.com>
2021-01-10 21:33:23 +0700

    update 10/01/21

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from 'react'
-import { Text, View, Button } from 'react-native'
+import { Text, View, Button, FlatList, ActivityIndicator } from 'react-native'
 import { Header } from '../components'
 
 const Home = ({ navigation }) => {
     const [contoh, setContoh] = useState('nilai awal')
     const [show, setShow] = useState(false)    
+    const [provinces, setProvinces] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        fetch('https://dev.farizdotid.com/api/daerahindonesia/provinsi')
+        .then(res => res.json())
+        .then(data => setProvinces(data.provinsi))
+        .catch(err => alert(err))
+        .finally(() => setLoading(false))
+    }, [contoh])
+
     return (
         <>
             <Header title="Home" noBack />
@@ -19,8 +31,18 @@ const Home = ({ navigation }) => {
                 <Button title="Munculkan" onPress={() => setShow( nowValue => !nowValue )} />
                 
                 <Button title="Ke Form" onPress={() => navigation.navigate('Form')} />
+
+                <FlatList 
+                    data={provinces}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({item}) => (
+                        <Text>{item.nama}</Text>
+                    )}
+                />
                 
             </View>
+
+            {loading && <ActivityIndicator size="large" color="blue" style={{ position: 'absolute', elevation: 20, top: 10, left: 0, right: 0 }} />}
         </>
     )
 }
